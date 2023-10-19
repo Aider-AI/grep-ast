cac4aba41e41e88551931e862ba84bb340777c1f
Paul Gauthier <github-repos@paulg.org>
2023-10-19 10:14:41 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+import re
+
 from setuptools import find_packages, setup
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    long_description = re.sub(r"\n!\[.*\]\(.*\)", "", long_description)
+
 setup(
     name="grep-ast",
     version="0.1.0",
     description="A tool to grep through the AST of a source file",
+    url="https://github.com/paul-gauthier/grep-ast",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
