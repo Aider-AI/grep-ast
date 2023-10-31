b79a47f140ae77b9a820156dc9a024f8353def33
Paul Gauthier <github-repos@paulg.org>
2023-10-31 16:36:19 -0700

    Added tests for all parsers

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,5 +1,8 @@
-import pytest
-from grep_ast.parsers import PARSERS, get_parser
+from tree_sitter_languages import get_parser
+
+from grep_ast.dump import dump  # noqa: F401
+from grep_ast.parsers import PARSERS
+
 
 def test_get_parser_for_all_parsers():
     for lang in PARSERS.values():
